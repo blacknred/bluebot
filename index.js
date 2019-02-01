1f8e12afe2b54a2e0068822e6398016b9118aa14
blcknrd <tretyakov.dmi@gmail.com>
2019-02-02 01:35:32 +0300

    webhooks

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
 
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const PORT = process.env.PORT || 3001;
+const URL = process.env.URL || 'https://blooobot.herokuapp.com';
+
 const bot = new Telegraf(process.env.BOT_TOKEN, {
     username: 'bloobot'
 });
@@ -9,9 +13,12 @@ bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears(/./, (ctx) => ctx.reply(ctx.message));
 
 bot.command('oldschool', (ctx) => ctx.reply('Hello'));
 bot.command('modern', ({ reply }) => reply('Yo'));
 bot.command('hipster', Telegraf.reply('λ'));
 
-bot.launch();
+// bot.launch();
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT)
