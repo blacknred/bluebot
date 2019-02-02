12b486c4ee6fb98a69878c4aa8bafe757662e9b0
blcknrd <tretyakov.dmi@gmail.com>
2019-02-02 05:34:08 +0300

    procfile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const Telegraf = require('telegraf');
 const HttpsProxyAgent = require('https-proxy-agent');
+const fs = require('fs')
 
 dotenv.config();
 const PORT = process.env.PORT || 3333;
@@ -34,7 +35,16 @@ bot.command('modern', ({ reply }) => reply('Yo'));
 bot.command('hipster', Telegraf.reply('λ'));
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears('rpic', (ctx) => ctx.reply('Hey there'));
+bot.hears('rpic', (ctx) => ctx.replyWithPhoto({
+    url: 'https://source.unsplash.com/collection/3356631/600x800',
+    filename: 'kitten.jpg'
+}));
+// bot.hears('rvid', async (ctx) => ctx.replyWithVideo({
+//     source: await fs.createReadStream('https://video-hw.xvideos-cdn.com/videos/3gp/d/0/3/xvideos.com_d03f939ac75ac1df76e037d573919a74.mp4?e=1549084178&ri=1024&rs=85&h=00063f7067cc9aa79a7833e2a4cfcd86')
+// }));
+
+
+
 bot.hears(/./, (ctx) => ctx.reply( `${ctx.message.text} from ${ctx.message.chat.first_name}`));
 
 
