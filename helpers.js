cf764f44d6d270b35907a1fe774ad1699c7d1edf
blcknrd <tretyakov.dmi@gmail.com>
2019-02-04 13:48:09 +0300

    horoscopes, advices, dad jokes, should

--- helpers.js
+++ helpers.js
@@ -1,21 +1,70 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
+const GAG_URL = 'http://9gag.com';
+const YESNO_URL = 'https://yesno.wtf/api';
+const POL_URL = 'https://2ch.hk/po/threads.json';
+const DAD_JOKE_URL = 'https://icanhazdadjoke.com/';
 const ADVICE_URL = 'https://api.adviceslip.com/advice';
 const HORO_URL = 'http://horoscope-api.herokuapp.com/horoscope';
 const RAND_IMAGE_URL = 'https://source.unsplash.com/collection/3356631/600x800';
+const GAG_SECTIONS = ['comic', 'country', 'nsfw'];
 const HORO_DATES = ['today', 'week', 'month', 'year'];
-const HORO_SIGNS = ['Taurus', 'Leo', 'Scorpio', 'Aquarius','Gemini', 'Virgo', 'Sagittarius', 'Pisces', 'Aries', 'Cancer', 'Libra', 'Capricorn'];
+const HORO_SUNSIGNS = ['taurus', 'leo', 'scorpio', 'aquarius', 'gemini', 'virgo',
+    'sagittarius', 'pisces', 'aries', 'cancer', 'libra', 'capricorn',
+];
+const ANSWERS = {
+    nojoke: 'No, Dad didn`t joke about it yet.',
+    jokeerror: 'No time for jokes mate.',
+    noadvice: 'Sorry, nothing to advice.',
+    horoscopeerror: 'Let fate remain a mystery this time',
+};
+
+axios.defaults.timeout = 5000;
 
 function getRandSong() {
     return Buffer.alloc(800000);
 }
 
-function getRandImage() {
+async function getRandMeme() {
+    const index = Math.floor(Math.random() * GAG_SECTIONS.length);
+    const res = await axios.get(`${GAG_URL}/${GAG_SECTIONS[index]}/fresh`);
+
+    const $ = cheerio.load(res.data);
+    const result = $('a.badge-track').map((a) => {
+        console.log('kk');
+        console.log($(this));
+        var item = {
+            title: null,
+            id: null,
+            url: null,
+            image: null,
+            points: null,
+            commentCount: null
+        };
+        // item.title = $(this).children('header').children('h2.badge-item-title').children('a').text().trim();
+        // item.id = $(this).attr('data-entry-id');
+        // item.url = "http://9gag.com" + $(this).children('div.badge-post-container').children('a').attr('href');
+        // item.image = $(this).children('div.badge-post-container').children('a').children('img').attr('src');
+        // item.points = $(this).children('p.post-meta').children('a.badge-evt.point').children('span.badge-item-love-count').text().trim();
+        // item.commentCount = $(this).children('p.post-meta').children('a.badge-evt.comment').text().trim();
+        // item.commentCount = item.commentCount.substring(0, item.commentCount.indexOf(' comments'));
+
+        return item;
+    }).get();
+    console.log(result);
+    
+
     return RAND_IMAGE_URL;
 }
 
-function getRandPol() {
-    return 'https://2ch.hk/po/res/31651319.json';
+async function getPol() {
+    const res = await axios.get(POL_URL);
+    console.log(res.data.threads);
+    return `
+        ${res.data.threads[0].subject}
+        https://2ch.hk/po/res/${res.data.threads[0].num}.html
+    `;
 }
 
 async function getRandVideo() {
@@ -27,42 +76,84 @@ async function getRandVideo() {
     return res.data;
 }
 
-async function getRandomAdvice(text) {
-    const query = text.split(' ')[1];
-    const res = await axios({
-        url: `${ADVICE_URL}${query ? `/search/${query}` : ''}`,
-        responseType: 'json',
-    });
+// should adv horo dad 
+async function getYesNo() {
+    try {
+        const res = await axios.get(YESNO_URL);
+        return res.data.image || res.data.answer;
+    } catch (e) {
+        return ['No', 'Yes', 'Maybe..'][Math.floor(Math.random() * 3)];
+    }
+}
 
-    if (res.data.message) return 'Sorry, nothing to advice.';
-    if (!query) return res.data.slip.advice;
-    const randIndex = Math.floor(Math.random() * res.data.slips.length);
-    return res.data.slips[randIndex].advice;
+async function getAdvice(text) {
+    const query = text.split(' ')[1];
+
+    try {
+        const res = await axios.get(`${ADVICE_URL}${query ? `/search/${query}` : ''}`);
+    
+        if (!res || res.data.message) throw new Error();
+        if (!query) return res.data.slip.advice;
+        const randIndex = Math.floor(Math.random() * res.data.slips.length);
+        return res.data.slips[randIndex].advice;
+    } catch (e) {
+        return ANSWERS.noadvice;
+    }
+}
+
+async function getJoke(text) {
+    const query = text.split(' ')[1];
+
+    try {
+        const res = await axios({
+            url: `${DAD_JOKE_URL}${query ? `search?term=${query}` : ''}`,
+            responseType: 'json',
+            headers: {
+                Accept: 'text/plain',
+            }
+        });
+
+        if (!res.data) return ANSWERS.nojoke;
+        if (!query) return res.data;
+        const dataArr = res.data.split('\n');
+        const randIndex = Math.floor(Math.random() * dataArr.length);
+        return dataArr[randIndex];
+    } catch (e) {
+        return ANSWERS.jokeerror;
+    }
 }
 
 async function getHoroscope(text) {
     let datespan = 'today';
     let sunsign = 'Leo';
-
     const optsArr = text.split(' ').slice(1) || [];
     optsArr.forEach((opt) => {
+        opt = opt.toLowerCase();
         if (HORO_DATES.includes(opt)) datespan = opt;
-        if (HORO_SIGNS.includes(opt)) sunsign = opt;
+        if (HORO_SUNSIGNS.includes(opt)) sunsign = opt;
     });
+    sunsign = sunsign.charAt(0).toUpperCase() + sunsign.slice(1);
 
-    const res = await axios({
-        url: `${HORO_URL}/${datespan}/${sunsign}`,
-        responseType: 'json',
-    });
+    try {
+        const res = await axios({
+            url: `${HORO_URL}/${datespan}/${sunsign}`,
+            responseType: 'json',
+        });
 
-    return res.data.horoscope;
+        const responce = res.data.horoscope.replace(/Ganesha/g, 'Astrologist');
+        return responce;
+    } catch (e) {
+        return ANSWERS.horoscopeerror;
+    } 
 }
 
 module.exports = {
-    getRandPol,
+    getJoke,
+    getAdvice,
+    getPol,
+    getYesNo,
     getRandSong,
     getRandVideo,
-    getRandImage,
+    getRandMeme,
     getHoroscope,
-    getRandomAdvice,
 };
--- middleware.js
+++ middleware.js
@@ -1,47 +1,78 @@
 // const Telegraf = require('telegraf');
+const translate = require('@vitalets/google-translate-api');
 
 const {
-    getRandPol,
+    getJoke,
+    getYesNo,
+    getAdvice,
+    getPol,
     getRandSong,
     getRandVideo,
-    getRandImage,
+    getRandMeme,
     getHoroscope,
-    getRandomAdvice,
 } = require('./helpers');
 
 const ANSWERS = {
     welcome: 'Welcome! Random content grabber here)... Start with commands.',
+    hi: 'Hey there',
     help: 'Just random content buddy. Command.',
     weird: '👍 but I\'m waiting for the command.',
+    heavy: 'Heavy calculations...',
+    wait: 'Wait...',
+    error: 'Hmm...',
+    done: 'Done.',
 };
 
-function withAsyncResponse(ctx, next) {
+async function withAsyncResponse(ctx, next) {
+    const pre1 = setTimeout(() => ctx.reply(ANSWERS.wait), 500);
+    const pre2 = setTimeout(() => ctx.reply(ANSWERS.heavy), 6000);
     try {
-        next(ctx);
+        await next(ctx);
     } catch (e) {
-        ctx.reply(`Hmm..${e.message}`);
+        ctx.reply(`${ANSWERS.error}${e.message}`);
     } finally {
-        ctx.reply('Done. Wait...');
+        clearTimeout(pre1);
+        clearTimeout(pre2);
+        ctx.reply(ANSWERS.done);
+    }
+}
+
+async function replyWithTranslation(ctx, next) {
+    const text = await next(ctx);
+    try {
+        const res = await translate(text, {
+            from: 'en',
+            to: ctx.message.from.language_code,
+            proxies: [
+                process.env.HTTP_PROXY,
+            ]
+        });
+        ctx.reply(res.text);
+    } catch (e) {
+        ctx.reply(text);
     }
 }
 
 module.exports = (bot) => {
-    bot.command('rpic', withAsyncResponse, ctx => ctx.replyWithPhoto({
-        url: getRandImage(),
-    }));
-    bot.command('rmus', withAsyncResponse, async (ctx) => ctx.replyWithVoice({
-        source: await getRandSong(),
-    }));
-    bot.command('rvid', withAsyncResponse, async (ctx) => ctx.replyWithVideo({
-        source: await getRandVideo(),
-    }));
-    bot.command('pol', async (ctx) => ctx.reply(getRandPol()));
-    bot.command('adv', async (ctx) => ctx.reply(await getRandomAdvice(ctx.message.text)));
-    bot.command('horo', async (ctx) => ctx.reply(await getHoroscope(ctx.message.text)));
-    
     bot.start(ctx => ctx.reply(ANSWERS.welcome));
     bot.help(ctx => ctx.reply(ANSWERS.help));
+
+    bot.command('mus', withAsyncResponse, async (ctx) => ctx.replyWithVoice({
+        source: await getRandSong(),
+    }));
+    bot.command('vid', withAsyncResponse, async (ctx) => ctx.replyWithVideo({
+        source: await getRandVideo(),
+    }));
+    bot.command('meme', withAsyncResponse, async (ctx) => ctx.replyWithPhoto({
+        url: await getRandMeme(),
+    }));
+    bot.command('dad', replyWithTranslation, ctx => getJoke(ctx.message.text));
+    bot.command('adv', replyWithTranslation, ctx => getAdvice(ctx.message.text));
+    bot.command('horo', replyWithTranslation, ctx => getHoroscope(ctx.message.text));
+    bot.command('pol', withAsyncResponse, async(ctx) => ctx.reply(await getPol()));
+    bot.command('should', withAsyncResponse, async(ctx) => ctx.reply(await getYesNo()));
+    
     bot.on('sticker', ctx => ctx.reply(ANSWERS.weird));
-    bot.hears(/(hi|Hi|hello|Hello)+/, ctx => ctx.reply('Hey there'));
+    bot.hears(/(hi|Hi|hello|Hello)+/, ctx => ctx.reply(ANSWERS.hi));
     bot.hears(/./, ({ reply, message: m }) => reply(`${m.text} from ${m.chat.first_name}`));
 };
